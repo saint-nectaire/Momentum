972ce5e9154cb25ffd9dd83dd9278ed991f3a9f8
Casperavr <casper@hhc.earth>
2024-08-16 12:31:59 +0200

    initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
+      <h1>Saint-nectaire</h1>
 
+      <Routes>
+        <Route path="/" element={<Homepage/>} />
+        <Route path="/about" element={<Aboutpage/>} />
+        <Route path="/*" element={<Errorpage/>} />
+      </Routes>
     </>
   )
 }

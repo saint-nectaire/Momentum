6f650bdd1b652afe7e99b80f79a011a9e6aa6b88
Sam Merwan <sam.mjalaoui@gmail.com>
2024-08-19 10:39:46 +0200

    Update App component using mui layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,47 @@
-import { Routes, Route } from 'react-router-dom'
-import Homepage from "./pages/Homepage";
-import Aboutpage from "./pages/Aboutpage";
-import Errorpage from "./pages/Errorpage";
+import { Routes, Route } from 'react-router-dom';
+import Homepage from './pages/Homepage';
+import Aboutpage from './pages/Aboutpage';
+import Errorpage from './pages/Errorpage';
+import Footer from './components/Footer';
+import { Container, Box, Typography, CssBaseline } from '@mui/material';
 
 function App() {
-
   return (
     <>
-      <h1>Saint-nectaire</h1>
+      <CssBaseline /> 
+      <Container
+        component="main"
+        maxWidth="lg"
+        sx={{ 
+          display: 'flex', 
+          flexDirection: 'column', 
+          minHeight: '100vh', 
+        }}
+      >
+          <Typography variant="h1" component="h1">
+            Saint-nectaire
+          </Typography>
 
-      <Routes>
-        <Route path="/" element={<Homepage/>} />
-        <Route path="/about" element={<Aboutpage/>} />
-        <Route path="/*" element={<Errorpage/>} />
-      </Routes>
+        <Box
+          component="main"
+          sx={{ 
+            flex: 1, 
+            display: 'flex', 
+            justifyContent: 'center', 
+            alignItems: 'center' 
+          }}
+        >
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/about" element={<Aboutpage />} />
+            <Route path="/*" element={<Errorpage />} />
+          </Routes>
+        </Box>
+
+        <Footer />
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

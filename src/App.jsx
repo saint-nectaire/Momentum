384abf91d4685fe496fc16ff5ec958195a539ac3
Sam Merwan <sam.mjalaoui@gmail.com>
2024-08-20 10:37:50 +0200

    delete costum colors for the moment

--- src/App.jsx
+++ src/App.jsx
@@ -9,47 +9,7 @@ import { Container, CssBaseline } from '@mui/material';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { containerStyles } from './styles/styles';
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#998FC7", // Indigo
-    },
-    secondary: {
-      main: "#28262C", // Indigo
-    },
-    background: {
-      default: "#28262C", // Light Black
-      paper: "#998FC7",  // Indigo
-    },
-  },
-  typography: {
-    h1: {
-      color: "#998FC7", // Indigo
-    },
-    h4: {
-      color: "#E0E0E0", // Light Gray
-    },
-    subtitle1:{
-      color: "#E0E0E0", // Light Gray
-    },
-  },
-  components: {
-    MuiTextField: {
-      styleOverrides: {
-        root: {
-            backgroundColor: '#F0F0F0', // Light Gray
-        },
-      },
-    },
-    MuiSelect: {
-      styleOverrides: {
-        root: {
-          backgroundColor: '#F0F0F0', // Light Gray
-        },
-      },
-    },
-  },
-});
+const theme = createTheme({});
 
 
 function App() {

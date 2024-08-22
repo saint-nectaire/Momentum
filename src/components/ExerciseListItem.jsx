224681193ac0852a8125b74cc67be1a948f642fe
Sam Merwan <sam.mjalaoui@gmail.com>
2024-08-22 10:54:20 +0200

    update exercises icons

--- src/components/ExerciseListItem.jsx
+++ src/components/ExerciseListItem.jsx
@@ -1,27 +1,45 @@
 import { ListItemButton, ListItemIcon, ListItemText, Avatar, Divider, IconButton, Box } from '@mui/material';
 import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 import DeleteIcon from '@mui/icons-material/Delete';
+import DirectionsRunIcon from '@mui/icons-material/DirectionsRun';
+import EmojiPeopleIcon from '@mui/icons-material/EmojiPeople';
+import SportsGymnastics from '@mui/icons-material/SportsGymnastics';
+import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
+import ElectricBolt from '@mui/icons-material/ElectricBolt';
+
+
+const typeIconMapping = {
+    cardio: <DirectionsRunIcon />,
+    strength: <FitnessCenterIcon />,
+    olympic_weightlifting: <EmojiEventsIcon />,
+    plyometrics: <EmojiPeopleIcon />,
+    powerlifting: <ElectricBolt />,
+    stretching: <SportsGymnastics />,
+    strongman: <FitnessCenterIcon />,
+};
+
 
 function ExerciseListItem(props) {
+    const { exercise, onClick, onDelete } = props;
+    const exerciseIcon = typeIconMapping[exercise.type] || <FitnessCenterIcon />;
+
     return (
         <div>
-            <ListItemButton
-                onClick={() => props.onClick(props.exercise)}
-            >
+            <ListItemButton onClick={() => onClick(exercise)}>
                 <ListItemIcon>
                     <Avatar>
-                        <FitnessCenterIcon />
+                        {exerciseIcon}
                     </Avatar>
                 </ListItemIcon>
                 <ListItemText 
-                    primary={props.exercise.name || 'Unnamed Exercise'} 
-                    secondary={props.exercise.instructions || 'No instruction available'}
+                    primary={exercise.name || 'Unnamed Exercise'} 
+                    secondary={exercise.instructions || 'No instruction available'}
                 />
                 <Box>
                     <IconButton 
                         onClick={(e) => {
                             e.stopPropagation();
-                            props.onDelete(props.exercise.id);
+                            onDelete(exercise.id);
                         }} 
                         color="secondary"
                     >
